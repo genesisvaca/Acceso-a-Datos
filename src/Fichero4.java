80b20066749a2680d0a72c5ba52d4579c12a9c82
genesisvaca <genesisvaca034@gmail.com>
2025-10-01 16:19:19 +0200

    01102025 - Fichero 4, Listar ficheros y directorios

---
+++ src/Fichero4.java
@@ -0,0 +1,31 @@
+import java.io.File;
+
+public class Fichero4 {
+    public static void main(String[] args) {
+        // Directorio padre que acabamos de crear en la Ruta de documentos
+        File directorioPadre = new File("C:\\Users\\AlumnoAfternoon\\Documents\\Pruebas-Java");
+
+        // Nombre o ruta relativa al fichero que acabo de crear
+        String nomHijo = "hijo.txt";
+
+        // Creo una instancia File utilizando el constructor y la variable de arriba
+        File archivo = new File(directorioPadre, nomHijo);
+
+        // Verificar si el archivo existe
+        if (archivo.exists()){
+            // Si el archivo existe me muestra un mensaje en pantalla
+            // de que existe mostrando la  ruta completa especificada
+            System.out.println("El archivo existe en la ruta: " + archivo.getAbsolutePath());
+            for (String listDir : directorioPadre.list()){
+                if(archivo.isFile())
+                    System.out.println(listDir + " [ARCHIVO]");
+                else if (archivo.isDirectory())
+                    System.out.println(listDir + "[DIRECTORIO]");
+            }
+        } else {
+            // Si el archivo NO existe me muestra un mensaje en pantalla
+            // de que NO existe mostrando la  ruta completa especificada
+            System.out.println("El archivo NO existe en la ruta: " + archivo.getAbsolutePath());
+        }
+    }
+}
